4b42835199fbb4c1e8294a6277c44529cfa2a28e
Vineet Choudhary <vineetchoudhary@live.in>
2016-12-12 19:06:36 +0530

    App User Defaults

---
+++ AppBox/Common/UserData.m
@@ -0,0 +1,48 @@
+//
+//  UserData.m
+//  AppBox
+//
+//  Created by Vineet Choudhary on 12/12/16.
+//  Copyright © 2016 Developer Insider. All rights reserved.
+//
+
+#import "UserData.h"
+
+#define GmailLoggedIn @"GmailLoggedIn"
+#define UserEmail @"UserEmail"
+#define UserMessage @"UserMessage"
+
+@implementation UserData
+
+//Gmail Logged In
++(BOOL)isGmailLoggedIn{
+    return [[NSUserDefaults standardUserDefaults] boolForKey:GmailLoggedIn];
+}
+
++(void)setIsGmailLoggedIn:(BOOL)isGmailLoggedIn{
+    [[NSUserDefaults standardUserDefaults] setBool:isGmailLoggedIn forKey:GmailLoggedIn];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
++(NSString *)userEmail{
+    NSString *userEmail = [[NSUserDefaults standardUserDefaults] stringForKey:UserEmail];
+    return userEmail == nil ? @"" : userEmail;
+}
+
++(void)setUserEmail:(NSString *)userEmail{
+    [[NSUserDefaults standardUserDefaults] setValue:userEmail forKey:UserEmail];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
++(NSString *)userMessage{
+    NSString *userMessage = [[NSUserDefaults standardUserDefaults] stringForKey:UserMessage];
+    return userMessage == nil ? @"" : userMessage;
+}
+
++(void)setUserMessage:(NSString *)userMessage{
+    [[NSUserDefaults standardUserDefaults] setValue:userMessage forKey:UserMessage];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+
+@end
