8860317bb2bf35799bf338ef955d90ddc1de0308
Vineet Choudhary <vineetchoudhary@live.in>
2016-12-21 11:24:59 +0530

    add prefixes “ab”(means AppBox) in all constant

--- AppBox/Common/Constants.h
+++ AppBox/Common/Constants.h
@@ -10,20 +10,27 @@
 #define Constants_h
 
 //Base URL's
-#define MailerBaseURL @"https://tryapp.github.io/mail"
-#define GitHubLatestRelease @"https://api.github.com/repos/vineetchoudhary/AppBox-iOSAppsWirelessInstallation/releases/latest"
+#define abMailerBaseURL @"https://tryapp.github.io/mail"
+#define abGitHubLatestRelease @"https://api.github.com/repos/vineetchoudhary/AppBox-iOSAppsWirelessInstallation/releases/latest"
 
 //dropbox
-#define DbRoot kDBRootAppFolder
-#define DbAppkey @"86tfx5bu3356fqo"
-#define DbScreatkey @"mq4l1damoz8hwrr"
+#define abDbRoot kDBRootAppFolder
+#define abDbAppkey @"86tfx5bu3356fqo"
+#define abDbScreatkey @"mq4l1damoz8hwrr"
 
 //notification
-#define SessionLogUpdated @"SessionLogUpdated"
+#define abSessionLogUpdated @"SessionLogUpdated"
 
 //messages
-#define KeepSameLinkHelpTitle @"What is keep same link for all future upload?"
-#define KeepSameLinkHelpMessage  @"This feature will keep same short url for all future build/ipa uploaded with same bundle identifier, this means old build/ipa url will replaced by new ipa file. You can change the link by changing the Dropbox app folder name below. \n\nWe are working on this feature, Once we fineshed this you will be able to install previous build/ipa also."
+#define abKeepSameLinkHelpTitle @"What is keep same link for all future upload?"
+#define abKeepSameLinkHelpMessage  @"This feature will keep same short url for all future build/ipa uploaded with same bundle identifier, this means old build/ipa url will replaced by new ipa file. You can change the link by changing the Dropbox app folder name below. \n\nWe are working on this feature, Once we fineshed this you will be able to install previous build/ipa also."
+
+//team id constants
+#define abiPhoneDeveloper @"iphone developer"
+#define abiPhoneDistribution @"iphone distribution"
+#define abFullName @"fullName"
+#define abTeamName @"teamName"
+#define abTeamId @"teamId"
 
 
 //enums
