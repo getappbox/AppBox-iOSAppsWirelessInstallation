9f0df5904bb14a3ef15d502d1c4ea46016e59334
Vineet Choudhary <vineetchoudhary@live.in>
2017-01-12 16:50:53 +0530

    Commented GitHub updated code

--- AppBox/Common/UpdateHandler/UpdateHandler.m
+++ AppBox/Common/UpdateHandler/UpdateHandler.m
@@ -15,13 +15,23 @@
     @try {
         [[AppDelegate appDelegate] addSessionLog:@"Checking for new version..."];
         [NetworkHandler requestWithURL:abGitHubLatestRelease withParameters:nil andRequestType:RequestGET andCompletetion:^(id responseObj, NSError *error) {
+            //handle error and check for all required keys
             if (error == nil &&
                 [((NSDictionary *)responseObj).allKeys containsObject:@"tag_name"] &&
                 [((NSDictionary *)responseObj).allKeys containsObject:@"html_url"]){
+                
+                //get tag name, because it's always be latest version
                 NSString *tag = [responseObj valueForKey:@"tag_name"];
                 NSString *newVesion = [[tag componentsSeparatedByCharactersInSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]] componentsJoinedByString:abEmptyString];
+                
+                //get version string from bundle info.plist
                 NSString *versionString = [[[NSBundle mainBundle] infoDictionary] valueForKey:@"CFBundleShortVersionString"];
                 NSString *currentVersion = [[versionString componentsSeparatedByCharactersInSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]] componentsJoinedByString:abEmptyString];
+                
+                //log current and latest version
+                [[AppDelegate appDelegate] addSessionLog:[NSString stringWithFormat:@"Current Version - %@ <=> Latest Version - %@", versionString, tag]];
+                
+                //return result based on version strings
                 completion(([newVesion compare:currentVersion] == NSOrderedDescending),[NSURL URLWithString:[responseObj valueForKey:@"html_url"]]);
             }else{
                 completion(false, nil);
