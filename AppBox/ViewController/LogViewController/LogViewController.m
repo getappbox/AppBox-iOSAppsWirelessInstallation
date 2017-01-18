57f785b8eb3ee575d15a175da912c4faa4037d72
Vineet Choudhary <vineetchoudhary@live.in>
2017-01-18 19:09:42 +0530

    Disabling auto log scroll if user moving in up direction

--- AppBox/ViewController/LogViewController/LogViewController.m
+++ AppBox/ViewController/LogViewController/LogViewController.m
@@ -12,19 +12,41 @@
 
 @end
 
-@implementation LogViewController
+@implementation LogViewController{
+    BOOL isAutoScrollEnabled;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do view setup here.
     [self refreshLog];
+    isAutoScrollEnabled = YES;
     [Common logScreen:@"AppBox Log"];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshLog) name:abSessionLogUpdated object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleScroll:) name:NSScrollViewDidLiveScrollNotification object:nil];
 }
 
 -(void)refreshLog{
-    textView.string = [[AppDelegate appDelegate] sessionLog];
-    [textView scrollRangeToVisible:NSMakeRange(textView.string.length-1, 1)];
+    if (isAutoScrollEnabled){
+        textView.string = [[AppDelegate appDelegate] sessionLog];
+        [textView scrollRangeToVisible:NSMakeRange(textView.string.length-1, 1)];
+    }
+}
+
+-(void)handleScroll:(NSNotification *)notification{
+    if ([notification.object isKindOfClass:[NSScrollView class]] && ([notification.object isEqualTo:textView.enclosingScrollView])){
+        //get visiable and bound rect
+        NSRect  boundsRect = [textView bounds];
+        NSRect  visRect = [textView visibleRect];
+        
+        //Disable auto scroll
+        isAutoScrollEnabled = NO;
+        
+        //enable auto scroll if user scrolled to bottom
+        if (NSMaxY(visRect) - NSMaxY(boundsRect) == 0.0){
+            isAutoScrollEnabled = YES;
+        }
+    }
 }
 
 
