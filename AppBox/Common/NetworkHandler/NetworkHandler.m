eca5a3af81706110379367ea4d34486e383d27c6
Vineet Choudhary <vineetchoudhary@live.in>
2017-03-25 11:34:00 +0530

    Added resource handler for HTML request and simple POST request

--- AppBox/Common/NetworkHandler/NetworkHandler.m
+++ AppBox/Common/NetworkHandler/NetworkHandler.m
@@ -13,17 +13,48 @@
 
 +(void)requestWithURL:(NSString *)url withParameters:(id)parmeters andRequestType:(RequestType)requestType andCompletetion:(void (^)(id responseObj, NSError *error))completion{
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    manager.requestSerializer = [AFJSONRequestSerializer serializer];
-    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    [manager setRequestSerializer: [AFJSONRequestSerializer serializer]];
+    [manager setResponseSerializer: [AFJSONResponseSerializer serializer]];
     if (requestType == RequestGET){
-        [manager GET:url parameters:parmeters success:^(NSURLSessionDataTask *task, id responseObject) {
-             completion(responseObject, nil);
-        } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        [manager GET:url parameters:parmeters progress:^(NSProgress * _Nonnull downloadProgress) {
+            [[AppDelegate appDelegate] addSessionLog:@"Request In Progress!!"];
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            completion(responseObject, nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
             completion(nil, error);
         }];
     }else if (requestType == RequestPOST){
-        
+        [manager POST:url parameters:parmeters progress:^(NSProgress * _Nonnull uploadProgress) {
+            [[AppDelegate appDelegate] addSessionLog:@"Request In Progress!!"];
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            completion(responseObject, nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            completion(nil, error);
+        }];
     }
 }
 
++(void)getContentOfURL:(NSString *)url withParameters:(id)parmeters withRequestType:(RequestType)requestType andCompletetion:(void (^)(id responseObj, NSInteger statusCode, NSError *error))completion{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager setRequestSerializer: [AFJSONRequestSerializer serializer]];
+    [manager setResponseSerializer: [AFHTTPResponseSerializer serializer]];
+    [manager.responseSerializer setAcceptableContentTypes:[NSSet setWithObjects:@"application/javascript", @"text/html", @"text/javascript", nil]];
+    if (requestType == RequestGET){
+        [manager GET:url parameters:parmeters progress:^(NSProgress * _Nonnull downloadProgress) {
+            [[AppDelegate appDelegate] addSessionLog:@"Request In Progress!!"];
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            completion(responseObject, ((NSHTTPURLResponse *)task.response).statusCode ,nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            completion(nil, ((NSHTTPURLResponse *)task.response).statusCode ,error);
+        }];
+    }else if (requestType == RequestPOST){
+        [manager POST:url parameters:parmeters progress:^(NSProgress * _Nonnull uploadProgress) {
+            [[AppDelegate appDelegate] addSessionLog:@"Request In Progress!!"];
+        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            completion(responseObject, ((NSHTTPURLResponse *)task.response).statusCode ,nil);
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            completion(nil, ((NSHTTPURLResponse *)task.response).statusCode ,error);
+        }];
+    }
+}
 @end
