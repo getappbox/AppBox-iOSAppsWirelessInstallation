0cb8aeb6fe07d385834150fffdf06420f5027b01
Vineet Choudhary <vineetchoudhary@live.in>
2017-08-12 18:07:40 +0530

    Getting local system ip

---
+++ AppBox/Common/LocalServerHandler/LocalServerHandler.h
@@ -0,0 +1,15 @@
+//
+//  LocalServerHandler.h
+//  AppBox
+//
+//  Created by Vineet Choudhary on 12/08/17.
+//  Copyright © 2017 Developer Insider. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalServerHandler : NSObject
+
++(NSString *)getLocalIPAddress;
+
+@end
