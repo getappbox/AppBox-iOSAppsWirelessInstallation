4eac305899e05b9a2c310a33c6bca08b818b983b
Vineet Choudhary <vineetchoudhary@live.in>
2018-01-16 17:29:20 +0530

    Added xcpretty internally

---
+++ AppBox/Common/LogManager/ABLog.m
@@ -0,0 +1,13 @@
+//
+//  ABLog.m
+//  AppBox
+//
+//  Created by Vineet Choudhary on 16/01/18.
+//  Copyright © 2018 Developer Insider. All rights reserved.
+//
+
+#import "ABLog.h"
+
+@implementation ABLog
+
+@end
