08040b6317c241bd22bb7cb1fe69504d6157fe2d
Vineet Choudhary <vineetchoudhary@live.in>
2018-10-31 23:40:56 +0530

    Fixed build appstore issue for CI & CD

--- AppBox/Common/RepoBuilder/RepoBuilder.m
+++ AppBox/Common/RepoBuilder/RepoBuilder.m
@@ -116,25 +116,24 @@ NSString *const RepoITCPassword = @"itcpassword";
     }
     
     //app-store and itcmail check
-    BOOL isAppStoreBuild = [projectRawSetting.allKeys containsObject:RepoBuildTypeKey] && [project.buildType isEqualToString: BuildTypeAppStore];
-    if (isAppStoreBuild && ![projectRawSetting.allKeys containsObject:RepoITCEmail]) {
-        [[AppDelegate appDelegate] addSessionLog:[NSString stringWithFormat:@"iTunes Connect Account email not available in appbox.plist. You need to add \"%@\" key with iTubes Connect Account email id.", RepoITCEmail]];
+    if (![projectRawSetting.allKeys containsObject:RepoITCEmail]) {
+        [[AppDelegate appDelegate] addSessionLog:[NSString stringWithFormat:@"AppStore Connect Account email not available in appbox.plist. You need to add \"%@\" key with AppStore Connect Account email id in appbox.plist. Read more here - %@", RepoITCEmail, abCICDAppStore]];
         exit(abExitCodeITCMailNotFound);
     }
     
     //itcemail
-    if (isAppStoreBuild && [projectRawSetting.allKeys containsObject:RepoITCEmail]) {
+    if ([projectRawSetting.allKeys containsObject:RepoITCEmail]) {
         project.itcUserName = [projectRawSetting valueForKey:RepoITCEmail];
         if ([MailHandler isAllValidEmail:project.itcUserName]) {
             NSString *password = [SAMKeychain passwordForService:abiTunesConnectService account:project.itcUserName];
             if (password == nil || password.length == 0) {
-                [[AppDelegate appDelegate] addSessionLog:[NSString stringWithFormat:@"iTunes Connect Account %@ not available in keychain. Please add account in AppBox first.", project.itcUserName]];
+                [[AppDelegate appDelegate] addSessionLog:[NSString stringWithFormat:@"iTunes Connect Account %@ not available in keychain. Please add account in AppBox first. Read more here - %@", project.itcUserName, abCICDAppStore]];
                 exit(abExitCodeForInvalidITCAccount);
             } else {
                 project.itcPasswod = password;
             }
         } else if (project.itcUserName.length > 0) {
-            [[AppDelegate appDelegate] addSessionLog:[NSString stringWithFormat:@"iTunes Connect Account %@ not a valid email.", project.itcUserName]];
+            [[AppDelegate appDelegate] addSessionLog:[NSString stringWithFormat:@"iTunes Connect Account %@ not a valid email. Please enter a valid email address.", project.itcUserName]];
             exit(abExitCodeForInvalidITCAccount);
         }
     }
