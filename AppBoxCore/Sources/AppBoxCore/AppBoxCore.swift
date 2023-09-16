ec147dd63aa5acbd39e9da72e6e73e9dc0367c36
Vineet Choudhary <vineetchoudhary@live.in>
2023-09-16 14:56:11 +0530

    Add archive manager to unzip input ipa file

--- AppBoxCore/Sources/AppBoxCore/AppBoxCore.swift
+++ AppBoxCore/Sources/AppBoxCore/AppBoxCore.swift
@@ -5,13 +5,28 @@
 //  Created by Vineet Choudhary on 19/01/23.
 //
 
+import Foundation
+
 public struct AppBoxCore {
+	private let fileManager = FileManager.default
+	private let archiveManager: ArchiveManager
+
 	private let launchArguments: LaunchArguments
+
 	public init(launchArguments: LaunchArguments) {
 		self.launchArguments = launchArguments
+
+		// static initializer
+		Log.initialize()
+
+		// dependency initializer
+		self.archiveManager = DefaultArchiveManager(fileManager: fileManager)
+
+		Log.debug(launchArguments.description)
     }
 
-	public func upload() {
-		print("Uploading \(launchArguments.ipaPath)")
+	public func upload() async throws {
+		let archiveFiles = try await archiveManager.unzip(file: launchArguments.ipaURL)
+		Log.debug(archiveFiles.description)
 	}
 }
