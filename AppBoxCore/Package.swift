341a7c448ea8a3360f5d1f5663fa3d8b4d04b366
Vineet Choudhary <vineetchoudhary@live.in>
2023-09-16 14:48:31 +0530

    Add ZIPFoundation and swift-log dependency

--- AppBoxCore/Package.swift
+++ AppBoxCore/Package.swift
@@ -16,14 +16,18 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+		.package(url: "https://github.com/weichsel/ZIPFoundation.git", branch: "development"),
+		.package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppBoxCore",
-            dependencies: []),
+            dependencies: [
+				.byName(name: "ZIPFoundation"),
+				.product(name: "Logging", package: "swift-log")
+			]),
         .testTarget(
             name: "AppBoxCoreTests",
             dependencies: ["AppBoxCore"]),
