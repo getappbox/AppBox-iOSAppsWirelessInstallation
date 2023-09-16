128f6e76118ee30bc833b69aa679741c78a97d48
Vineet Choudhary <vineetchoudhary@live.in>
2023-09-16 15:16:26 +0530

    Add protocol for AppInfoManager for mainfest and appInfo file

---
+++ AppBoxCore/Sources/AppBoxCore/AppInfoManager/AppInfoManager.swift
@@ -0,0 +1,16 @@
+//
+//  AppInfoManager.swift
+//
+//
+//  Created by Vineet Choudhary on 16/09/23.
+//
+
+import Foundation
+
+protocol AppInfoManager {
+	init(archiveFiles: ArchiveFiles)
+
+	func manifestFile(ipa: URL) -> URL
+	
+	func appInfoFile(mainfest: URL) -> URL
+}
---
+++ AppBoxCore/Sources/AppBoxCore/AppInfoManager/DefaultAppInfoManager/DefaultAppInfoManager.swift
@@ -0,0 +1,26 @@
+//
+//  DefaultAppInfoManager.swift
+//  
+//
+//  Created by Vineet Choudhary on 16/09/23.
+//
+
+import Foundation
+
+final class DefaultAppInfoManager: AppInfoManager {
+	private let archiveFiles: ArchiveFiles
+
+	init(archiveFiles: ArchiveFiles) {
+		self.archiveFiles = archiveFiles
+	}
+	
+	func manifestFile(ipa: URL) -> URL {
+		return ipa
+	}
+	
+	func appInfoFile(mainfest: URL) -> URL {
+		return mainfest
+	}
+	
+	
+}
