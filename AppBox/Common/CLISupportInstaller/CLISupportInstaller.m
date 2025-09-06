75c9b8f833690afc5d06beab73075201d6702ba5
Vineet Choudhary <vineetchoudhary@live.in>
2025-09-06 13:42:59 +0530

    Add AppBoxCLI project with CLI Support Installer

---
+++ AppBox/Common/CLISupportInstaller/CLISupportInstaller.m
@@ -0,0 +1,49 @@
+//
+//  CLISupportInstaller.h
+//  AppBox
+//
+//  Created by Vineet Choudhary on 30/12/16.
+//  Copyright © 2016 Developer Insider. All rights reserved.
+//
+
+#import "CLISupportInstaller.h"
+
+@implementation CLISupportInstaller
+
++ (BOOL)install {
+    NSString * const kInstallerName = @"install.sh";
+    NSString * const kCLIName = @"appboxcli";
+    NSString * const kCLIInstallPath = [NSString stringWithFormat:@"/usr/local/bin/%@", kCLIName];
+
+    AuthorizationRef authorization;
+    OSStatus status = AuthorizationCreate(NULL, NULL, kAuthorizationFlagDefaults, &authorization);
+    if (status == errAuthorizationSuccess) {
+        AuthorizationItem items = {kAuthorizationRightExecute, 0, NULL, 0};
+        AuthorizationRights rights = {1, &items};
+        status = AuthorizationCopyRights(authorization, &rights, NULL, kAuthorizationFlagDefaults | kAuthorizationFlagInteractionAllowed | kAuthorizationFlagExtendRights | kAuthorizationFlagPreAuthorize, NULL);
+        if (status == errAuthorizationSuccess) {
+            NSString *sharedSupportPath = NSBundle.mainBundle.sharedSupportPath;
+            NSString *installerPath = [sharedSupportPath stringByAppendingPathComponent:kInstallerName];
+            NSString *toolPath = [sharedSupportPath stringByAppendingPathComponent:kCLIName];
+            char *arguments[] = {(char*)toolPath.fileSystemRepresentation, (char*)kCLIInstallPath.fileSystemRepresentation, NULL};
+            FILE *communicationPipe = NULL;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            status = AuthorizationExecuteWithPrivileges(authorization, installerPath.fileSystemRepresentation, kAuthorizationFlagDefaults, arguments, &communicationPipe);
+#pragma clang diagnostic pop
+            if (status == errAuthorizationSuccess) {
+                char buffer[128];
+                ssize_t count = read(fileno(communicationPipe), buffer, sizeof(buffer));
+                NSData *data = [[NSData alloc] initWithBytes:buffer length:count];
+                NSString *result = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+                if (![result isEqualToString:@"OK"]) {
+                    status = -1;  // Code doesn't matter, just not errAuthorizationSuccess
+                }
+            }
+        }
+        AuthorizationFree(authorization, kAuthorizationFlagDefaults);
+    }
+    return status == errAuthorizationSuccess;
+}
+
+@end
