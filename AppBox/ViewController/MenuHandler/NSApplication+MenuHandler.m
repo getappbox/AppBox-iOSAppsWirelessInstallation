aa78d8b6433bb1d93c86d772fba9b9e151829d83
Vineet Choudhary <vineetchoudhary@live.in>
2025-09-06 15:40:46 +0530

    Move CLI install alert in install support helper class

--- AppBox/ViewController/MenuHandler/NSApplication+MenuHandler.m
+++ AppBox/ViewController/MenuHandler/NSApplication+MenuHandler.m
@@ -130,28 +130,11 @@
 
 - (IBAction)cliInstallUninstallTapped:(NSMenuItem *)sender {
 	if ([[NSFileManager defaultManager] fileExistsAtPath:abCLIPath]){
-		//uninstall
-		NSAlert *alert = [[NSAlert alloc] init];
-		[alert setMessageText: @"Are you sure?"];
-		[alert setInformativeText:@"Do you want to uninstall AppBox CLI tool?"];
-		[alert setAlertStyle:NSAlertStyleInformational];
-		[alert addButtonWithTitle:@"Yes"];
-		[alert addButtonWithTitle:@"No"];
-		if ([alert runModal] == NSAlertFirstButtonReturn){
-			NSError *error;
-			if ([CLISupportHelper uninstall]){
-				[Common showAlertWithTitle:@"Success" andMessage:@"AppBox CLI tool uninstalled successfully."];
-				[EventTracker logEventWithType: LogEventTypeCLIUninstall];
-			}else{
-				[Common showAlertWithTitle:@"Error" andMessage:[NSString stringWithFormat:@"Failed to uninstall AppBox CLI tool.\n\n%@", error.localizedDescription]];
-				[EventTracker logEventWithType: LogEventTypeCLIUninstallError];
-			}
-			[self updateCLIMenu];
-		}
+		[CLISupportHelper uninstall];
 	} else {
 		[CLISupportHelper install];
-		[self updateCLIMenu];
 	}
+	[self updateCLIMenu];
 }
 
 
