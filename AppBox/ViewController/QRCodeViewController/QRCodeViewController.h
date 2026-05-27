3ae5681e3f91e09e5f393e88b0c47108138d68bc
Vineet Choudhary <vineetchoudhary@live.in>
2026-05-27 19:34:20 +0530

    Replace ZXingObjC with CoreImage

--- AppBox/ViewController/QRCodeViewController/QRCodeViewController.h
+++ AppBox/ViewController/QRCodeViewController/QRCodeViewController.h
@@ -7,7 +7,6 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#import <ZXingObjC/ZXingObjC.h>
 
 @interface QRCodeViewController : NSViewController{
     __weak IBOutlet NSImageView *imageViewQRCode;
--- AppBox/ViewController/QRCodeViewController/QRCodeViewController.m
+++ AppBox/ViewController/QRCodeViewController/QRCodeViewController.m
@@ -7,13 +7,14 @@
 //
 
 #import "QRCodeViewController.h"
+#import <CoreImage/CoreImage.h>
 
 @implementation QRCodeViewController{
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
     //create qr code and show in imageview
     NSString *url;
     if (self.ipaUploadInfo) {
@@ -23,18 +24,45 @@
     } else {
         url = @"No URL found.";
     }
-    NSError *error = nil;
-    ZXMultiFormatWriter *writer = [ZXMultiFormatWriter writer];
-    ZXBitMatrix *result = [writer encode:url format:kBarcodeFormatQRCode width:250 height:250 error:&error];
-    if (result){
-        ZXImage *zxImage = [ZXImage imageWithMatrix: result
-                                            onColor: NSColor.labelColor.CGColor
-                                           offColor: NSColor.clearColor.CGColor];
-        NSImage *image = [[NSImage alloc] initWithCGImage:[zxImage cgimage] size:NSMakeSize(250, 250)];
+    NSImage *image = [self qrCodeImageForString:url size:NSMakeSize(250, 250)];
+    if (image){
         [imageViewQRCode setImage:image];
     }else{
-        [Common showAlertWithTitle:@"Error" andMessage:error.localizedDescription];
+        [Common showAlertWithTitle:@"Error" andMessage:@"Unable to generate QR code."];
     }
+}
+
+- (NSImage *)qrCodeImageForString:(NSString *)string size:(NSSize)size {
+    NSData *data = [string dataUsingEncoding:NSISOLatin1StringEncoding];
+    if (data == nil) {
+        return nil;
+    }
+
+    CIFilter *qrFilter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    [qrFilter setValue:data forKey:@"inputMessage"];
+    [qrFilter setValue:@"M" forKey:@"inputCorrectionLevel"];
+    CIImage *qrImage = qrFilter.outputImage;
+    if (qrImage == nil) {
+        return nil;
+    }
+
+    // Map the black data modules to the label color and the white background to clear.
+    CIFilter *colorFilter = [CIFilter filterWithName:@"CIFalseColor"];
+    [colorFilter setValue:qrImage forKey:kCIInputImageKey];
+    [colorFilter setValue:[CIColor colorWithCGColor:NSColor.labelColor.CGColor] forKey:@"inputColor0"];
+    [colorFilter setValue:[CIColor colorWithCGColor:NSColor.clearColor.CGColor] forKey:@"inputColor1"];
+    CIImage *coloredImage = colorFilter.outputImage ?: qrImage;
+
+    // Scale up using nearest-neighbour sampling so the modules stay crisp.
+    CGRect extent = coloredImage.extent;
+    CGFloat scale = MIN(size.width / extent.size.width, size.height / extent.size.height);
+    CIImage *scaledImage = [[coloredImage imageBySamplingNearest]
+                            imageByApplyingTransform:CGAffineTransformMakeScale(scale, scale)];
+
+    NSCIImageRep *rep = [NSCIImageRep imageRepWithCIImage:scaledImage];
+    NSImage *image = [[NSImage alloc] initWithSize:size];
+    [image addRepresentation:rep];
+    return image;
 }
 
 - (IBAction)buttonCloseTapped:(NSButton *)sender {
