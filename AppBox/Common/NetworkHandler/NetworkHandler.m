397db6ec887711b1163f4041011b6452cd918f73
Vineet Choudhary <vineetchoudhary@live.in>
2026-05-27 19:35:54 +0530

    Replace AFNetworking with NSURLSession

--- AppBox/Common/NetworkHandler/NetworkHandler.m
+++ AppBox/Common/NetworkHandler/NetworkHandler.m
@@ -8,65 +8,121 @@
 
 #import "NetworkHandler.h"
 
-
 @implementation NetworkHandler
 
+//MARK: - JSON request, JSON response
+
 +(void)requestWithURL:(NSString *)url withParameters:(id)parmeters andRequestType:(RequestType)requestType andCompletetion:(void (^)(id responseObj, NSInteger httpStatus, NSError *error))completion{
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    [manager setRequestSerializer: [AFJSONRequestSerializer serializer]];
-    [manager setResponseSerializer: [AFJSONResponseSerializer serializer]];
-    if (requestType == RequestGET){
-        [manager GET:url parameters:parmeters headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
-            DDLogDebug(@"Request In Progress -  %@", url);
-        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-            NSInteger statusCode = ((NSHTTPURLResponse *)task.response).statusCode;
-            completion(responseObject, statusCode, nil);
-        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-            NSInteger statusCode = ((NSHTTPURLResponse *)task.response).statusCode;
-            if (statusCode == HTTP_OK) {
-                completion(@"ok", statusCode, nil);
-            }else{
-                completion(nil, statusCode, error);
-            }
-        }];
-    }else if (requestType == RequestPOST){
-        [manager POST:url parameters:parmeters headers:nil progress:^(NSProgress * _Nonnull uploadProgress) {
-            DDLogDebug(@"Request In Progress -  %@", url);
-        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-            NSInteger statusCode = ((NSHTTPURLResponse *)task.response).statusCode;
-            completion(responseObject, statusCode, nil);
-        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-            NSInteger statusCode = ((NSHTTPURLResponse *)task.response).statusCode;
-            if (statusCode == HTTP_OK) {
-                completion(@"ok", statusCode, nil);
-            }else{
-                completion(nil, statusCode, error);
-            }
-        }];
+    NSMutableURLRequest *request = [self jsonRequestWithURL:url parameters:parmeters requestType:requestType];
+    if (request == nil) {
+        [self deliverResponse:nil status:0 error:[self errorWithCode:NSURLErrorBadURL message:@"Invalid request."] toCompletion:completion];
+        return;
     }
+
+    DDLogDebug(@"Request In Progress -  %@", url);
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSInteger statusCode = [response isKindOfClass:[NSHTTPURLResponse class]] ? ((NSHTTPURLResponse *)response).statusCode : 0;
+
+        // Parse the body as JSON (mirrors AFJSONResponseSerializer).
+        id responseObject = nil;
+        if (data.length > 0) {
+            responseObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        }
+
+        BOOL httpSuccess = (statusCode >= 200 && statusCode < 300);
+        if (httpSuccess && responseObject != nil) {
+            // 2xx with a valid JSON body.
+            [self deliverResponse:responseObject status:statusCode error:nil toCompletion:completion];
+        } else if (statusCode == HTTP_OK) {
+            // 200 with a non-JSON body (e.g. Slack "ok" / Teams "1").
+            [self deliverResponse:@"ok" status:statusCode error:nil toCompletion:completion];
+        } else {
+            [self deliverResponse:nil status:statusCode error:(error ?: [self errorWithCode:statusCode message:@"Request failed."]) toCompletion:completion];
+        }
+    }];
+    [task resume];
 }
 
+//MARK: - JSON request, raw (HTTP) response
+
 +(void)getContentOfURL:(NSString *)url withParameters:(id)parmeters withRequestType:(RequestType)requestType andCompletetion:(void (^)(id responseObj, NSInteger statusCode, NSError *error))completion{
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    [manager setRequestSerializer: [AFJSONRequestSerializer serializer]];
-    [manager setResponseSerializer: [AFHTTPResponseSerializer serializer]];
-    [manager.responseSerializer setAcceptableContentTypes:[NSSet setWithObjects:@"application/javascript", @"text/html", @"text/javascript", nil]];
-    if (requestType == RequestGET){
-        [manager GET:url parameters:parmeters  headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
-            DDLogDebug(@"Request In Progress -  %@", url);
-        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-            completion(responseObject, ((NSHTTPURLResponse *)task.response).statusCode ,nil);
-        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-            completion(nil, ((NSHTTPURLResponse *)task.response).statusCode ,error);
-        }];
-    }else if (requestType == RequestPOST){
-        [manager POST:url parameters:parmeters headers:nil progress:^(NSProgress * _Nonnull uploadProgress) {
-            DDLogDebug(@"Request In Progress -  %@", url);
-        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-            completion(responseObject, ((NSHTTPURLResponse *)task.response).statusCode ,nil);
-        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-            completion(nil, ((NSHTTPURLResponse *)task.response).statusCode ,error);
-        }];
+    NSMutableURLRequest *request = [self jsonRequestWithURL:url parameters:parmeters requestType:requestType];
+    if (request == nil) {
+        [self deliverResponse:nil status:0 error:[self errorWithCode:NSURLErrorBadURL message:@"Invalid request."] toCompletion:completion];
+        return;
     }
+
+    DDLogDebug(@"Request In Progress -  %@", url);
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSInteger statusCode = [response isKindOfClass:[NSHTTPURLResponse class]] ? ((NSHTTPURLResponse *)response).statusCode : 0;
+        BOOL httpSuccess = (statusCode >= 200 && statusCode < 300);
+        if (httpSuccess && error == nil) {
+            [self deliverResponse:data status:statusCode error:nil toCompletion:completion];
+        } else {
+            [self deliverResponse:nil status:statusCode error:(error ?: [self errorWithCode:statusCode message:@"Request failed."]) toCompletion:completion];
+        }
+    }];
+    [task resume];
 }
+
+//MARK: - Helpers
+
++ (NSMutableURLRequest *)jsonRequestWithURL:(NSString *)urlString parameters:(id)parameters requestType:(RequestType)requestType {
+    NSString *finalURLString = urlString;
+    NSData *bodyData = nil;
+
+    if (requestType == RequestPOST) {
+        if (parameters != nil) {
+            NSError *jsonError = nil;
+            bodyData = [NSJSONSerialization dataWithJSONObject:parameters options:0 error:&jsonError];
+            if (jsonError != nil) {
+                return nil;
+            }
+        }
+    } else if ([parameters isKindOfClass:[NSDictionary class]] && [(NSDictionary *)parameters count] > 0) {
+        NSString *query = [self queryStringFromDictionary:parameters];
+        NSString *separator = ([urlString rangeOfString:@"?"].location == NSNotFound) ? @"?" : @"&";
+        finalURLString = [NSString stringWithFormat:@"%@%@%@", urlString, separator, query];
+    }
+
+    NSURL *url = [NSURL URLWithString:finalURLString];
+    if (url == nil) {
+        return nil;
+    }
+
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = (requestType == RequestPOST) ? @"POST" : @"GET";
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    if (bodyData != nil) {
+        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+        request.HTTPBody = bodyData;
+    }
+    return request;
+}
+
++ (NSString *)queryStringFromDictionary:(NSDictionary *)dictionary {
+    NSMutableCharacterSet *allowed = [[NSCharacterSet URLQueryAllowedCharacterSet] mutableCopy];
+    [allowed removeCharactersInString:@"&=?+"];
+    NSMutableArray<NSString *> *pairs = [NSMutableArray array];
+    for (id key in dictionary) {
+        NSString *encodedKey = [[key description] stringByAddingPercentEncodingWithAllowedCharacters:allowed];
+        NSString *encodedValue = [[dictionary[key] description] stringByAddingPercentEncodingWithAllowedCharacters:allowed];
+        [pairs addObject:[NSString stringWithFormat:@"%@=%@", encodedKey, encodedValue]];
+    }
+    return [pairs componentsJoinedByString:@"&"];
+}
+
++ (NSError *)errorWithCode:(NSInteger)code message:(NSString *)message {
+    return [NSError errorWithDomain:@"AppBoxNetworkHandler" code:code userInfo:@{NSLocalizedDescriptionKey: message}];
+}
+
++ (void)deliverResponse:(id)responseObj status:(NSInteger)status error:(NSError *)error toCompletion:(void (^)(id, NSInteger, NSError *))completion {
+    if (completion == nil) {
+        return;
+    }
+    dispatch_async(dispatch_get_main_queue(), ^{
+        completion(responseObj, status, error);
+    });
+}
+
 @end
